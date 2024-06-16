0b3586af75e1a6cf55503833c9090f8717e1bb0b
Yuji Yamaguchi <3946829+yug1224@users.noreply.github.com>
2024-06-16 20:30:31 +0900

    Initial commit

---
+++ background.js
@@ -0,0 +1,77 @@
+// background.js
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: contentScriptFunc,
+  });
+});
+
+async function contentScriptFunc() {
+  // 任意の時間待機する
+  const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // ボタンをクリックする
+  const click = async (el) => {
+    el && (el.click() || console.log(el));
+    await sleep(200);
+  };
+
+  // Qiitaのいいね、ストック、フォローをクリックする
+  const qiitaFn = async () => {
+    const a = Array.from(
+      document.querySelectorAll(
+        'div.p-items_main > div:nth-child(1) button[aria-label="いいねする"],div.p-items_main > div:nth-child(1) button[aria-label="ストックする"]',
+      ),
+    );
+    const b = Array.from(
+      document.querySelectorAll('div.p-items_main > div:nth-child(2) button'),
+    ).filter((v) => v.textContent === 'フォロー');
+    for await (const v of [...a, ...b]) {
+      await click(v);
+    }
+  };
+
+  // Zennのいいね、フォローをクリックする
+  const zennFn = async () => {
+    const a = document.querySelector('#share button[aria-label="いいね"][class^="LikeButton_button__"]');
+    const b = Array.from(
+      document.querySelectorAll('div[class^="Container_default__"] button[class^="FollowButton_button__"]'),
+    ).filter((v) => v.textContent === 'フォロー');
+    for await (const v of [a, ...b]) {
+      await click(v);
+    }
+  };
+
+  // noteのいいね、フォローをクリックする
+  const noteFn = async () => {
+    const a = document.querySelector('div.p-article__action button[aria-label="スキ"]');
+    const b = document.querySelector('#sideCreatorProfile > span > span > button[data-type="primary"]');
+    for await (const v of [a, b]) {
+      await click(v);
+    }
+  };
+
+  // Speaker Deckのいいね、フォローをクリックする
+  const speakerdeckFn = async () => {
+    const a = document.querySelector('.deck a[data-method="post"][href$="/star"]');
+    const b = document.querySelector('.deck a[data-method="post"][href^="/connections/"]');
+    for await (const v of [a, b]) {
+      await click(v);
+    }
+  };
+
+  // サービスを判別する
+  if (location.origin === 'https://qiita.com') {
+    await qiitaFn();
+  } else if (location.origin === 'https://zenn.dev') {
+    await zennFn();
+  } else if (
+    location.origin === 'https://note.com' || !!document.querySelector('a[aria-label="home"][href="https://note.com/"]')
+  ) {
+    await noteFn();
+  } else if (location.origin === 'https://speakerdeck.com') {
+    await speakerdeckFn();
+  } else {
+    // その他のサイトの場合
+  }
+}
